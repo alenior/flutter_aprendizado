59a8831f61c21bf65d44dec8796cf7b623429821
alenior <alenior@gmail.com>
2025-01-21 19:52:56 -0300

    Vídeo 10

--- projeto_aprendizado/lib/main.dart
+++ projeto_aprendizado/lib/main.dart
@@ -1,46 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_aprendizado/app_widget.dart';
 
 main() {
-  runApp(AppWidget(title: 'Texto exemplo'));
-}
-
-class AppWidget extends StatelessWidget {
-  final String title;
-
-  const AppWidget({super.key, required this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.red),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  State<HomePage> createState() {
-    return HomePageState();
-  }
-}
-
-class HomePageState extends State<HomePage> {
-  int counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: GestureDetector(
-          child: Text('Contador: $counter'),
-          onTap: () {
-            setState(() {
-              counter++;
-            });
-          },
-        ),
-      ),
-    );
-  }
+  runApp(AppWidget());
 }
