d00b8de65e7915edca9794b5a686dbee876e12ae
alenior <alenior@gmail.com>
2025-02-11 21:11:48 -0300

    Atualização.

--- api_consulta_cep/lib/main.dart
+++ api_consulta_cep/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Consulta CEP',
+      title: 'Consulta CEP - Desenvolvido por Alencar Júnior',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
